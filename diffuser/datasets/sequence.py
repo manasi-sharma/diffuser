c53401ea62c135e77f005d8bd393f54eb694796c
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-02-28 20:58:56 -0800

    update

--- diffuser/datasets/sequence.py
+++ diffuser/datasets/sequence.py
@@ -17,33 +17,55 @@ class SequenceDataset(torch.utils.data.Dataset):
 
     def __init__(self, env='hopper-medium-replay', horizon=64,
         normalizer='LimitsNormalizer', preprocess_fns=[], max_path_length=1000,
-        max_n_episodes=10000, termination_penalty=0, use_padding=True, seed=None):
+        max_n_episodes=10000, termination_penalty=0, use_padding=True, seed=None, 
+        use_npy_inputs=False):
         self.preprocess_fn = get_preprocess_fn(preprocess_fns, env)
-        self.env = env = load_environment(env)
-        self.env.seed(seed)
         self.horizon = horizon
         self.max_path_length = max_path_length
         self.use_padding = use_padding
-        itr = sequence_dataset(env, self.preprocess_fn)
 
-        fields = ReplayBuffer(max_n_episodes, max_path_length, termination_penalty)
-        for i, episode in enumerate(itr):
-            fields.add_path(episode)
-        fields.finalize()
+        self.use_npy_inputs = use_npy_inputs
 
-        self.normalizer = DatasetNormalizer(fields, normalizer, path_lengths=fields['path_lengths'])
-        self.indices = self.make_indices(fields.path_lengths, horizon)
+        if self.use_npy_inputs:
+            """Reading in the .npy files (saved data format)"""
+            normed_observations = np.load('/iliad/u/manasis/language-diffuser/code/dataset_npy_files/normed_observations.npy')
+            normed_actions = np.load('/iliad/u/manasis/language-diffuser/code/dataset_npy_files/normed_actions.npy')
+            language = np.load('/iliad/u/manasis/language-diffuser/code/dataset_npy_files/language.npy')
 
-        self.observation_dim = fields.observations.shape[-1]
-        self.action_dim = fields.actions.shape[-1]
-        self.fields = fields
-        self.n_episodes = fields.n_episodes
-        self.path_lengths = fields.path_lengths
-        self.normalize()
+            fields = {}
+            fields['observations'] = normed_observations
+            fields['actions'] = normed_actions
+            fields['language'] = language
+            
+            self.observation_dim = fields['observations'].shape[-1]
+            self.action_dim = fields['actions'].shape[-1]
+            self.fields = fields
+            self.n_episodes = fields['observations'].shape[0]
+        else:
+            self.env = env = load_environment(env)
+            self.env.seed(seed)
+            itr = sequence_dataset(env, self.preprocess_fn)
 
-        print(fields)
-        # shapes = {key: val.shape for key, val in self.fields.items()}
-        # print(f'[ datasets/mujoco ] Dataset fields: {shapes}')
+            fields = ReplayBuffer(max_n_episodes, max_path_length, termination_penalty)
+            for i, episode in enumerate(itr):
+                fields.add_path(episode)
+            fields.finalize()
+
+            import pdb;pdb.set_trace()
+
+            self.normalizer = DatasetNormalizer(fields, normalizer, path_lengths=fields['path_lengths'])
+            self.indices = self.make_indices(fields.path_lengths, horizon)
+
+            self.observation_dim = fields.observations.shape[-1]
+            self.action_dim = fields.actions.shape[-1]
+            self.fields = fields
+            self.n_episodes = fields.n_episodes
+            self.path_lengths = fields.path_lengths
+            self.normalize()
+
+            print(fields)
+            # shapes = {key: val.shape for key, val in self.fields.items()}
+            # print(f'[ datasets/mujoco ] Dataset fields: {shapes}')
 
     def normalize(self, keys=['observations', 'actions']):
         '''
