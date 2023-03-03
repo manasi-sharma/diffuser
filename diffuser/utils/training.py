3500f378fe0bbc2b8f774fdcb4b2b162d218deec
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-03-02 19:53:42 -0800

    update

--- diffuser/utils/training.py
+++ diffuser/utils/training.py
@@ -109,6 +109,7 @@ class Trainer(object):
             for i in range(self.gradient_accumulate_every):
                 batch = next(self.dataloader)
                 batch = batch_to_device(batch)
+                import pdb;pdb.set_trace()
 
                 loss, infos = self.model.loss(*batch)
                 loss = loss / self.gradient_accumulate_every
